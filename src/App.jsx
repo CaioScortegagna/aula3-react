20023c828a1795a0123a7351ba9a8eca78eab620
CaioScortegagna <129218941+CaioScortegagna@users.noreply.github.com>
2023-11-21 15:29:06 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+function App() {
+  const [numero, setNumero ] = useState(0)
+  return (
+    <>
+    <button onClick={()=> setNumero(numero+1)}>+</button>
+    <p>{numero}</p>
+    <button onClick={()=> setNumero(numero-1)}>-</button>
+
+    </>
+  )
+}
+
+export default App
